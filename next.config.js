d2eb68c3b1009fa0cef46b1e7461ccd462a16984
Mateo Piñeiro <mateopineiro2001@gmail.com>
2023-08-14 17:19:25 -0300

    mvp

--- next.config.js
+++ next.config.js
@@ -1,4 +1,18 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  experimental: {
+    serverActions: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: '"avatars.githubusercontent.com"',
+        port: "",
+        pathname: "/*/**",
+      },
+    ],
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
